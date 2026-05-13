d2d7f559599b7e2ffaf4734213ee073f3a028d3e
NBNStacked <153414505+NBNStacked@users.noreply.github.com>
2026-05-13 13:16:23 -0700

    Update

---
+++ Session Insertions.sql
@@ -0,0 +1,33 @@
+use tutorsession;
+
+INSERT INTO STUDENT
+	VALUES (1, "Ed Sample", "edsamp@school.edu", "Chemistry"),
+    (2, "Gina Sample", "ginasamp@school.edu", "Education"),
+    (3, "Bob Newman", "newbob@school.edu", "Mechanical Engineering"),
+    (4, "Noel Goodguy", "guygood@school.edu", "Computer Science"),
+    (5, "Saul Goodman", "bettercalsaul@school.edu", "Law");
+INSERT INTO COURSE
+	VALUES (100, "Digital Forensics", "Computer Science"),
+    (110, "Historical Revisions and Statutes", "Law"),
+    (120, "Educational Technology", "Education"),
+    (130, "Thermodynamics", "Mechanical Engineering"),
+    (140, "Organic Chemistry I", "Chemistry"),
+    (150, "Virtual Reality", "Computer Science")
+;
+INSERT INTO ROOM
+	VALUES (100, "Sciences", 50),
+    (200, "Building of Law", 32),
+    (300, "Education and Arts", 44),
+    (110, "Sciences", 100),
+    (120, "Sciences", 100),
+    (130, "Sciences", 64)
+;
+INSERT INTO TUTOR
+	VALUES (1, "Steve Knowitall", "knowsit@school.edu"),
+    (2, "Gene Yuss", "genethegenius@school.edu"),
+    (3, "Samuel Smartson", "smartysam@school.edu"),
+    (4, "Ed Yucation", "ed_yucated@school.edu"),
+    (5, "Worm Bookson", "book_worm@school.edu"),
+    (6, "Tua Tea", "tua_tea@school.edu")
+
+;
